da49dd64abdcfab6bb282220cd627319bec181de
Michael Karpitsky <michael.karpitsky@gmail.com>
2011-05-08 16:56:33 +0300

    settings file

---
+++ settings.py
@@ -0,0 +1,19 @@
+import os
+
+API_URL = 'https://api.twitter.com/1'
+LANG = "ru"
+REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
+ACCESS_TOKEN_URL  = 'https://api.twitter.com/oauth/access_token'
+AUTHORIZATION_URL = 'https://api.twitter.com/oauth/authorize'
+SIGNIN_URL        = 'https://api.twitter.com/oauth/authenticate'
+
+ROOT_PATH = os.path.dirname(__file__)
+TEMPLATE_PATH = ROOT_PATH + "/template/"
+
+CONSUMER_KEY = "EU81HIQogu6U9UERPcB7Q"
+CONSUMER_SECRET	= "exyyZBSLYjsmZNaS8aAvv125A66NFvxIKvaHXzxM0"
+
+# data for access by bot
+TOKEN_KEY = ("284412415-Hc0DnpEcRDE5HggVe3ZXabLEPlseT2Nyj7Wn530A", "286305103-WFvbYZYsYyigTjvnRVtFG3dWbqsv5fogGF4Gy5Na")
+TOKEN_SECRET = ("4Qteql7AFXyleVBoobOhyQqSyErHvPBObcvGBgFHs","4WlM4h57TcX584dTrHu6p5SZw7w51Q4EQtok60zjcMA")
+USERID = "284412415"
